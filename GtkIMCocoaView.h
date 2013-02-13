849b3d8fe5ceb7b947b01b67215f2eb6be50ad8d
Takuro Ashie <ashie@clear-code.com>
2013-02-13 18:59:02 +0900

    Add include guard.

--- GtkIMCocoaView.h
+++ GtkIMCocoaView.h
@@ -1,6 +1,11 @@
+#ifndef __GTK_IM_COCOA_VIEW_H__
+#define __GTK_IM_COCOA_VIEW_H__
+
 #include <gdk/gdkquartz.h>
 
 @interface GtkIMCocoaView : NSView <NSTextInputClient> {
 }
 
 @end
+
+#endif /* __GTK_IM_COCOA_VIEW_H__ */
--- GtkIMCocoaWindowDelegate.h
+++ GtkIMCocoaWindowDelegate.h
@@ -1,4 +1,9 @@
+#ifndef __GTK_IM_COCOA_WINDOW_DELEGATE_H__
+#define __GTK_IM_COCOA_WINDOW_DELEGATE_H__
+
 #include <gdk/gdkquartz.h>
 
 @interface GtkIMCocoaWindowDelegate : NSObject <NSWindowDelegate>
 @end
+
+#endif /* __GTK_IM_COCOA_WINDOW_DELEGATE_H__ */
