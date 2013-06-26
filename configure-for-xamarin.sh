5bcc37f80767762f95f6232f0f706a3a90cdeb23
Takuro Ashie <ashie@clear-code.com>
2013-06-26 16:10:02 +0900

    Add a script to run configure against Xamarin Studio

---
+++ configure-for-xamarin.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+MONO_PATH="/Library/Frameworks/Mono.framework"
+GTK_PATH=`ls -d ${MONO_PATH}/Versions/2.10.* | sort -r | head -1`
+
+if test -z $GTK_PATH; then
+  echo "You don't have proper GTK+ in ${MONO_PATH}"
+  exit 1
+fi
+
+./configure --enable-workaround PATH="$GTK_PATH/bin:$PATH" PKG_CONFIG_PATH="$GTK_PATH/lib/pkgconfig" CFLAGS="-arch i386" LDFLAGS="-arch i386"
